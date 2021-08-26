4de9d58f3f1757d5d8bbfa6c3a8cd00dbb9ada60
Gabriel-Rabeloo <gabriel872003@gmail.com>
2021-08-26 19:07:50 -0300

    Alguns resolvers de teste

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String
+      hi: String
     }
   `,
+  resolvers: {
+    Query: {
+      hello: async () => {
+        return 'Hello again';
+      },
+      hi: () => {
+        return 'hi';
+      },
+    },
+  },
 });
 
 server.listen(4003).then(({ url }) => {
