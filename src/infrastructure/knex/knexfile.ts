6914b2ec0d2ade4ebc4eb99a91a1bdd9f280f22b
Gabriel-Rabeloo <gabriiel.rabeloo@gmail.com>
2022-02-19 17:09:07 -0300

    Heroku 1

--- src/infrastructure/knex/knexfile.ts
+++ src/infrastructure/knex/knexfile.ts
@@ -11,6 +11,8 @@ const config: { [key: string]: Knex.Config } = {
     client: process.env.DATABASE_CLIENT,
     connection: {
       database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT) || 3306,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
     },
@@ -28,6 +30,8 @@ const config: { [key: string]: Knex.Config } = {
     client: process.env.DATABASE_CLIENT,
     connection: {
       database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT) || 3306,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
     },
