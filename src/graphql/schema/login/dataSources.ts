4fea4957ab85b4cfc339c18456f54e672ef5c3f6
Gabriel-Rabeloo <gabriiel.rabeloo@gmail.com>
2022-02-26 13:36:20 -0300

    Fix cookie config

--- src/graphql/schema/login/dataSources.ts
+++ src/graphql/schema/login/dataSources.ts
@@ -39,7 +39,7 @@ export class LoginApi extends RESTDataSource {
       httpOnly: true, // Não deve ser acessado via código
       maxAge: process.env.COOKIE_MAX_AGE || 1000 * 60 * 60 * 24 * 7, // 7 days
       path: '/',
-      sameSite: 'strict', // strict lax none
+      sameSite: 'none', // strict lax none
     });
 
     return { userId, token };
